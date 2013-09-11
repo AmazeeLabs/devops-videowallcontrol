54f158eac8dec0becfe4dda94942846a4e95ca16
Bastian Widmer <b.widmer@dasrecht.net>
2013-09-11 08:57:50 +0200

    Removing unused stuff

--- init.sh
+++
@@ -1,2 +0,0 @@
-ssh vv4 ./serverboard &
-ssh vv5 ./generalboard &
--- turnoff.sh
+++
@@ -1,8 +0,0 @@
-echo -n -e "\xAA\x11\x01\x01\x00\x13" | nc 10.0.1.250 1515
-echo -n -e "\xAA\x11\x02\x01\x00\x14" | nc 10.0.1.250 1515
-echo -n -e "\xAA\x11\x03\x01\x00\x15" | nc 10.0.1.250 1515
-echo -n -e "\xAA\x11\x04\x01\x00\x16" | nc 10.0.1.250 1515
-echo -n -e "\xAA\x11\x05\x01\x00\x17" | nc 10.0.1.250 1515
-echo -n -e "\xAA\x11\x06\x01\x00\x18" | nc 10.0.1.250 1515
-echo -n -e "\xAA\x11\x07\x01\x00\x19" | nc 10.0.1.250 1515
-echo -n -e "\xAA\x11\x08\x01\x00\x1A" | nc 10.0.1.250 1515
